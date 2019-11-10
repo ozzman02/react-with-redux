9b44e723a3800cd715845226d6e1249bd934b3d2
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-11-10 14:49:47 -0600

    learning the basics

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,13 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+    const buttonText = { text: 'Click Me!' };
+    const labelText = 'Enter name: ';
+    const style = { backgroundColor: 'blue', color: 'white' };
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text"/>
+            <button style={style}>{buttonText.text}</button>
+        </div>
+    ); 
 }
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
