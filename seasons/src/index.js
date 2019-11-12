6ce94d5d7d78e86434fb4237f6ef18fb6268a042
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-11-11 20:10:50 -0600

    getting user's physical location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        /** success function callback */ 
+        (position) => {console.log(position)},
+        /** failure function callback */
+        (err) => {console.log(err)}
+    );
     return <div>Hi there !</div>;
 };
 
