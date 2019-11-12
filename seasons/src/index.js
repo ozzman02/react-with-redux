7b4dd5bb2ace4b77dc46e269fe46ce34abc27d5c
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-11-11 21:30:11 -0600

    updating State Properties

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        /** success function callback */ 
-        (position) => {console.log(position)},
-        /** failure function callback */
-        (err) => {console.log(err)}
-    );
-    return <div>Hi there !</div>;
-};
+
+class App extends React.Component {
+
+    constructor(props) {
+        
+        super(props);
+        
+        /** This is the only time when we do direct assignment to this.state */
+        this.state = { lat: null };
+        
+        window.navigator.geolocation.getCurrentPosition(
+            /** success function callback */ 
+            position => {
+                /** we called setState */
+                this.setState({ lat: position.coords.latitude });
+                
+                /** do not do 
+                 * this.state.lat = position.coords.latitude
+                */
+            },
+            /** failure function callback */
+            err => console.log(err)
+        );
+    }
+
+    render() {
+        
+    return <div>Latitude: {this.state.lat}</div>
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
