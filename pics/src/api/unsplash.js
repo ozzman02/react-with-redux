03b5f182093700c60288690de3cbde984752d532
Oscar Santamaría Venegas <oscar.santamaria@avantica.net>
2019-11-24 23:34:36 -0600

    finish app

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID b0ce22e08331fdd00b7a552538ed53e4366e62c06eaf73c38231670368998cd6'
+        Authorization: 'Client-ID b0ce22e08331fdd00b7a552538ed53e4366e62c06eaf73c38231670368998cd6',
+        "Set-Cookie": 'HttpOnly;Secure;SameSite=Strict'
     }
 });
 
