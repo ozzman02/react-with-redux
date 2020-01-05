d52fd5c2cc257d9bfaf2d22f25ef2f9bb957702d
oscar.santamaria <osantamaria@gmail.com>
2020-01-05 16:28:46 -0600

    adding RTMP server

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -14,12 +14,14 @@ class StreamShow extends React.Component {
             return <div>Loading</div>;
         }
 
+        /*
+            <h1>{this.props.stream.title}</h1>
+            <h5>{this.props.stream.description}</h5>
+         */
         const { title, description } = this.props.stream;
 
         return (
             <div>
-                {/*<h1>{this.props.stream.title}</h1>
-                <h5>{this.props.stream.description}</h5>*/}
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
