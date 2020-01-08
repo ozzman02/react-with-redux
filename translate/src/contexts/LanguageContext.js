eaaffb0a83239ecdddcf625eb1b49de83204faf7
oscar.santamaria <osantamaria@gmail.com>
2020-01-07 20:38:06 -0600

    Context System

---
+++ translate/src/contexts/LanguageContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+/*
+    The default value can be an array or an object.
+    We can share any information that we want.
+ */
+export default React.createContext('english');
