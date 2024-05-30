439e90c1349d59b856bbfbcacab5be83002d02a7
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-05-30 10:48:16 -0600

    from the scratch

--- translate/src/contexts/LanguageContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-/*
-    The default value can be an array or an object.
-    We can share any information that we want.
- */
-export default React.createContext('english');
