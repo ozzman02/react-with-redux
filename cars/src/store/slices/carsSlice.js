d28f0941b1afebb1af990a960524f8f305a4c823
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-08-11 19:55:59 -0600

    managing multiple slices with redux toolkit

---
+++ cars/src/store/slices/carsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        cars: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        addCar(state, action) {
+            /* Assumption: action.payload === { name: 'carName', cost: 140 } */
+            state.cars.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid()
+            });
+        },
+        removeCar(state, action) {
+            /* Assumption: action.payload === the id of the car we want to remove */
+            const updated = state.cars.filter((car) => {
+                /* If the comparison returns true, we want to keep the value if not, value is not added */
+                return car.id !== action.payload
+            });
+            state.cars = updated;
+        }
+    }
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+
+export const carsReducer = carsSlice.reducer;
