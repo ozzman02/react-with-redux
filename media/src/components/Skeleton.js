e2d66123eff64935779081e4f2620048a901cfb9
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-08-17 15:52:12 -0600

    interfacing with APIs using async thunks II

--- media/src/components/Skeleton.js
+++ media/src/components/Skeleton.js
@@ -1,6 +1,27 @@
 import classNames from "classnames";
 
-function Skeleton({ times }) {
+function Skeleton({ times, className }) {
+
+    const outerClassNames = classNames(
+        'relative',
+        'overflow-hidden',
+        'bg-gray-200',
+        'rounded',
+        'mb-2.5',
+        className
+    );
+
+    const innerClassNames = classNames(
+        'animate-shimmer',
+        'absolute',
+        'inset-0',
+        '-translate-x-full',
+        'bg-gradient-to-r',
+        'from-gray-200',
+        'via-white',
+        'to-gray-200'
+    );
+
     /*
         Original implementation to get the boxes:
 
@@ -10,8 +31,14 @@ function Skeleton({ times }) {
 
         _ means that we do not care that argument.
     */
-   const boxes = Array(times).fill(0).map((_, i) => {
-        return <div key={i} />;
+   const boxes = Array(times)
+    .fill(0)
+    .map((_, i) => {
+        return (
+            <div key={i} className={outerClassNames}>
+                <div className={innerClassNames} />
+            </div>
+        );
    });
 
    return boxes;
