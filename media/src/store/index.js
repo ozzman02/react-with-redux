c4d3a7df92e79f90da878bbe68c0f06ba97fe103
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-08-24 21:46:11 -0600

    modern async with redux toolkit query

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
+/* 
+    albums: albumsApi.reducer is equivalent to [albumsApi.reducerPath]: albumsApi.reducer
+*/
 export const store = configureStore({
     reducer: {
-        users: usersReducer
+        users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer 
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(albumsApi.middleware);
     }
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
 
+export { useFetchAlbumsQuery } from './apis/albumsApi';
